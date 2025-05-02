ae6d2bfbe681b15cefa14563e20c61a0e3e61e6b
Suresh Babu Gogula <55544386+sureshbabu1995@users.noreply.github.com>
2025-05-02 17:15:03 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suresh Sample Site</title>
+</head>
+<body>
+    <h1><?php echo "This is a sample site by Suresh G"; ?></h1>
+</body>
+</html>
